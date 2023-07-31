4ebfd04d42042ca4c26270778d7ad3a959b09161
SeemithaS <seemitha.xworkz@gmail.com>
2023-07-31 19:52:54 +0530

    Sports and Players

---
+++ Association/src/com/xworkz/sport/app/Lift.java
@@ -0,0 +1,15 @@
+package com.xworkz.sport.app;
+
+public class Lift {
+	
+	public int liftNo;
+	
+	public Lift(int liftNo) {
+		this.liftNo=liftNo;
+	}
+	
+	public void printInfo() {
+		System.out.println("Lift Numbe is: "+liftNo);
+	}
+
+}
